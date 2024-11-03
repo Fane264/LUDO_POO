27583eb59b098bfdef33d6365b127c0060ff95df
Fane264 <nastasestefan2004@gmail.com>
2024-11-03 11:58:45 +0200

    v0.1

--- Headers/Board.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Fane on 11/3/2024.
-//
-
-#ifndef BOARD_H
-#define BOARD_H
-
-#endif //BOARD_H
--- Headers/Player.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Fane on 11/3/2024.
-//
-
-#ifndef PLAYER_H
-#define PLAYER_H
-
-#endif //PLAYER_H
